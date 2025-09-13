7833304089b6a3f3165ad4e1a5ffdcee01ff9e5d
Mohammad Nasyikh Al-Qusyairy <nasyikhqusyairy@gmail.com>
2025-09-13 15:03:58 +0700

    Refactor autoloading and session handling in index.php; remove unused lines and improve comments for clarity

--- public/index.php
+++ public/index.php
@@ -1,24 +1,18 @@
 <?php
 
-// autoloading
-
 use App\Core\Route;
 
+// autoloading
 require_once __DIR__ . '/../vendor/autoload.php';
 
 // bootstrapping
 require_once __DIR__ . '/../libs/helpers.php';
 require_once __DIR__ . '/../routes/web.php';
 
-// load .env
-
-
 // untuk keperluan session (flash message, old input, login, dsb)
 session_start();
 
-// Ambil request
+// handle request
 $requestUri = trim(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH), '/');
 $requestMethod = $_SERVER['REQUEST_METHOD'];
-
-// Dispatch router
 Route::dispatch($requestUri, $requestMethod);
